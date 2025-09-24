c5b318647df9c1fda3da711c823d44aa9af56ed3
Daniel <pinedadpt@gmail.com>
2025-09-24 15:02:52 -0600

    Added user_key to Users table

--- postgreSQL/init/init.sql
+++ postgreSQL/init/init.sql
@@ -1,25 +1,7 @@
-CREATE TABLE "Users" (
-    "id" integer NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "email" varchar(255) NOT NULL,
-    "password_hash" text NOT NULL,
-    "phone" varchar(20),
-    "active" boolean NOT NULL,
-    "created_at" timestamp NOT NULL,
-    CONSTRAINT "pk_Users_id" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "Roles" (
-    "id" integer NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "description" text,
-    CONSTRAINT "pk_Roles_id" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "UserRoles" (
+CREATE TABLE "TeamUsers" (
+    "team_id" integer NOT NULL,
     "user_id" integer NOT NULL,
-    "role_id" integer NOT NULL,
-    CONSTRAINT "pk_user_roles" PRIMARY KEY ("user_id", "role_id")
+    CONSTRAINT "pk_team_users" PRIMARY KEY ("team_id", "user_id")
 );
 
 CREATE TABLE "Clients" (
@@ -30,6 +12,13 @@ CREATE TABLE "Clients" (
     "company" varchar(100),
     "created_at" timestamp,
     CONSTRAINT "pk_Clients_id" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Roles" (
+    "id" integer NOT NULL,
+    "name" varchar(100) NOT NULL,
+    "description" text,
+    CONSTRAINT "pk_Roles_id" PRIMARY KEY ("id")
 );
 
 CREATE TABLE "Projects" (
@@ -44,27 +33,16 @@ CREATE TABLE "Projects" (
     CONSTRAINT "pk_Projects_id" PRIMARY KEY ("id")
 );
 
-CREATE TABLE "Teams" (
-    "team_id" integer NOT NULL,
-    "project_id" integer NOT NULL,
-    "name" varchar(100) NOT NULL,
-    CONSTRAINT "pk_Teams_team_id" PRIMARY KEY ("team_id")
-);
-
-CREATE TABLE "TeamUsers" (
-    "team_id" integer NOT NULL,
-    "user_id" integer NOT NULL,
-    "project_role" varchar(100),
-    CONSTRAINT "pk_team_users" PRIMARY KEY ("team_id", "user_id")
-);
-
-CREATE TABLE "Notifications" (
+CREATE TABLE "Users" (
     "id" integer NOT NULL,
-    "user_id" integer NOT NULL,
-    "message" text,
-    "read" boolean,
-    "date" timestamp,
-    CONSTRAINT "pk_Notifications_id" PRIMARY KEY ("id")
+    "name" varchar(100) NOT NULL,
+    "email" varchar(255) NOT NULL,
+    "password_hash" text NOT NULL,
+    "phone" varchar(20),
+    "active" boolean NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "user_key" varchar,
+    CONSTRAINT "pk_Users_id" PRIMARY KEY ("id")
 );
 
 CREATE TABLE "Messages" (
@@ -74,6 +52,21 @@ CREATE TABLE "Messages" (
     "content" text,
     "date" timestamp,
     CONSTRAINT "pk_Messages_id" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "UserRoles" (
+    "user_id" integer NOT NULL,
+    "role_id" integer NOT NULL,
+    CONSTRAINT "pk_user_roles" PRIMARY KEY ("user_id", "role_id")
+);
+
+CREATE TABLE "Notifications" (
+    "id" integer NOT NULL,
+    "user_id" integer NOT NULL,
+    "message" text,
+    "read" boolean,
+    "date" timestamp,
+    CONSTRAINT "pk_Notifications_id" PRIMARY KEY ("id")
 );
 
 CREATE TABLE "Reports" (
@@ -88,17 +81,24 @@ CREATE TABLE "Reports" (
     CONSTRAINT "pk_Reports_id" PRIMARY KEY ("id")
 );
 
+CREATE TABLE "Teams" (
+    "team_id" integer NOT NULL,
+    "project_id" integer NOT NULL,
+    "name" varchar(100) NOT NULL,
+    CONSTRAINT "pk_Teams_team_id" PRIMARY KEY ("team_id")
+);
+
 -- Foreign key constraints
 -- Schema: public
-ALTER TABLE "UserRoles" ADD CONSTRAINT "fk_UserRoles_role_id_Roles_id" FOREIGN KEY("role_id") REFERENCES "Roles"("id");
-ALTER TABLE "UserRoles" ADD CONSTRAINT "fk_UserRoles_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
-ALTER TABLE "Projects" ADD CONSTRAINT "fk_Projects_team_leader_id_Users_id" FOREIGN KEY("team_leader_id") REFERENCES "Users"("id");
+ALTER TABLE "Messages" ADD CONSTRAINT "fk_Messages_project_id_Projects_id" FOREIGN KEY("project_id") REFERENCES "Projects"("id");
+ALTER TABLE "Messages" ADD CONSTRAINT "fk_Messages_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Notifications" ADD CONSTRAINT "fk_Notifications_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
+ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_project_id_Projects_id" FOREIGN KEY("project_id") REFERENCES "Projects"("id");
 ALTER TABLE "Projects" ADD CONSTRAINT "fk_Projects_client_id_Clients_id" FOREIGN KEY("client_id") REFERENCES "Clients"("id");
+ALTER TABLE "Projects" ADD CONSTRAINT "fk_Projects_team_leader_id_Users_id" FOREIGN KEY("team_leader_id") REFERENCES "Users"("id");
 ALTER TABLE "Teams" ADD CONSTRAINT "fk_Teams_project_id_Projects_id" FOREIGN KEY("project_id") REFERENCES "Projects"("id");
 ALTER TABLE "TeamUsers" ADD CONSTRAINT "fk_TeamUsers_team_id_Teams_team_id" FOREIGN KEY("team_id") REFERENCES "Teams"("team_id");
 ALTER TABLE "TeamUsers" ADD CONSTRAINT "fk_TeamUsers_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
-ALTER TABLE "Notifications" ADD CONSTRAINT "fk_Notifications_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
-ALTER TABLE "Messages" ADD CONSTRAINT "fk_Messages_project_id_Projects_id" FOREIGN KEY("project_id") REFERENCES "Projects"("id");
-ALTER TABLE "Messages" ADD CONSTRAINT "fk_Messages_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
-ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_project_id_Projects_id" FOREIGN KEY("project_id") REFERENCES "Projects"("id");
+ALTER TABLE "UserRoles" ADD CONSTRAINT "fk_UserRoles_role_id_Roles_id" FOREIGN KEY("role_id") REFERENCES "Roles"("id");
+ALTER TABLE "UserRoles" ADD CONSTRAINT "fk_UserRoles_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
 ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
