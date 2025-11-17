2c10e463142f3a654f437e35f3c5f707055d39dc
Daniel <pinedadpt@gmail.com>
2025-11-17 00:04:56 -0600

    fixed user report redundancy and connected notifications to projects

--- postgreSQL/init/init.sql
+++ postgreSQL/init/init.sql
@@ -1,29 +1,17 @@
-CREATE TABLE "Users" (
-    "id" integer NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "email" varchar(255) NOT NULL,
-    "password_hash" text NOT NULL,
-    "phone" varchar(20),
-    "active" boolean NOT NULL,
-    "created_at" timestamp NOT NULL,
-    "user_key" varchar(500),
-    "role" varchar(20),
-    "company" varchar(200),
-    CONSTRAINT "pk_Users_id" PRIMARY KEY ("id")
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'replaceme') THEN
+        PERFORM format('CREATE ROLE %I LOGIN PASSWORD %L', 'replaceme', 'replaceme');
+    END IF;
+END
+$$;
+GRANT USAGE ON SCHEMA public TO replaceme;
+GRANT CREATE ON SCHEMA public TO replaceme;
 
-CREATE TABLE "Clients" (
-    "id" integer NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "email" varchar(255),
-    "phone" varchar(20),
-    "company" varchar(100),
-    "created_at" timestamp,
-    CONSTRAINT "pk_Clients_id" PRIMARY KEY ("id")
-);
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO replaceme;
 
-CREATE TABLE "Projects" (
-    "id" integer NOT NULL,
+CREATE TABLE "projects" (
+    "id" varchar(8) NOT NULL,
     "name" varchar(100) NOT NULL,
     "description" text,
     "client_id" integer NOT NULL,
@@ -31,34 +19,47 @@ CREATE TABLE "Projects" (
     "start" timestamp,
     "end" timestamp,
     "database_path" varchar(255) NOT NULL,
-    CONSTRAINT "pk_Projects_id" PRIMARY KEY ("id")
+    CONSTRAINT "pk_projects_id" PRIMARY KEY ("id")
 );
 
-CREATE TABLE "Notifications" (
-    "id" integer NOT NULL,
-    "user_id" integer NOT NULL,
-    "message" text,
-    "read" boolean,
-    "date" timestamp,
-    CONSTRAINT "pk_Notifications_id" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "Reports" (
-    "id" integer NOT NULL,
+CREATE TABLE "reports" (
+    "id" serial NOT NULL,
     "project_id" integer NOT NULL,
     "user_id" integer NOT NULL,
     "type" varchar(50) NOT NULL,
     "parameters" jsonb,
     "format" varchar(20) NOT NULL,
-    "path" varchar(255),
+    "path" varchar(255) NOT NULL,
     "created_at" timestamp NOT NULL,
-    CONSTRAINT "pk_Reports_id" PRIMARY KEY ("id")
+    CONSTRAINT "pk_reports_id" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "users" (
+    "id" serial NOT NULL,
+    "name" varchar(100) NOT NULL,
+    "email" varchar(255) NOT NULL,
+    "password_hash" text NOT NULL,
+    "phone" varchar(20),
+    "active" boolean NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "user_key" varchar(32) NOT NULL,
+    "role" varchar(20) NOT NULL,
+    CONSTRAINT "pk_users_id" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "notifications" (
+    "id" serial NOT NULL,
+    "user_id" integer NOT NULL,
+    "message" text NOT NULL,
+    "read" boolean,
+    "date" timestamp NOT NULL,
+    "project_id" varchar NOT NULL,
+    CONSTRAINT "pk_notifications_id" PRIMARY KEY ("id")
 );
 
 -- Foreign key constraints
 -- Schema: public
-ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_project_id_Projects_id" FOREIGN KEY("project_id") REFERENCES "Projects"("id");
-ALTER TABLE "Notifications" ADD CONSTRAINT "fk_Notifications_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
-ALTER TABLE "Reports" ADD CONSTRAINT "fk_Reports_user_id_Users_id" FOREIGN KEY("user_id") REFERENCES "Users"("id");
-ALTER TABLE "Projects" ADD CONSTRAINT "fk_Projects_team_leader_id_Users_id" FOREIGN KEY("team_leader_id") REFERENCES "Users"("id");
-ALTER TABLE "Projects" ADD CONSTRAINT "fk_Projects_client_id_Clients_id" FOREIGN KEY("client_id") REFERENCES "Clients"("id");
+ALTER TABLE "notifications" ADD CONSTRAINT "fk_notifications_project_id_projects_id" FOREIGN KEY("project_id") REFERENCES "projects"("id");
+ALTER TABLE "reports" ADD CONSTRAINT "fk_reports_project_id_projects_id" FOREIGN KEY("project_id") REFERENCES "projects"("id");
+ALTER TABLE "notifications" ADD CONSTRAINT "fk_notifications_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "users"("id");
+ALTER TABLE "projects" ADD CONSTRAINT "fk_projects_team_leader_id_users_id" FOREIGN KEY("team_leader_id") REFERENCES "users"("id");
