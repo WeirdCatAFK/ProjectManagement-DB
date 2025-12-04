ae137105f401ffa05f378de9adaaf22ae5581ba8
Daniel <pinedadpt@gmail.com>
2025-12-03 18:52:47 -0600

    Updated scheme to fix the users table and project_members table

--- postgreSQL/init/init.sql
+++ postgreSQL/init/init.sql
@@ -21,28 +21,10 @@ GRANT CREATE ON SCHEMA public TO replaceme;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO replaceme;
 
-CREATE TABLE "projects" (
-    "id" varchar(8) NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "description" text,
-    "client_id" integer NOT NULL,
-    "team_leader_id" integer NOT NULL,
-    "start" timestamp,
-    "end" timestamp,
-    "database_path" varchar(255) NOT NULL,
-    CONSTRAINT "pk_projects_id" PRIMARY KEY ("id")
-);
-
-CREATE TABLE "reports" (
-    "id" serial NOT NULL,
-    "project_id" integer NOT NULL,
-    "user_id" integer NOT NULL,
-    "type" varchar(50) NOT NULL,
-    "parameters" jsonb,
-    "format" varchar(20) NOT NULL,
-    "path" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL,
-    CONSTRAINT "pk_reports_id" PRIMARY KEY ("id")
+CREATE TABLE "project_members" (
+    "project_id" varchar NOT NULL,
+    "user_id" serial,
+    CONSTRAINT "pk_table_5_id" PRIMARY KEY ("project_id", "user_id")
 );
 
 CREATE TABLE "users" (
@@ -58,33 +40,41 @@ CREATE TABLE "users" (
     CONSTRAINT "pk_users_id" PRIMARY KEY ("id")
 );
 
+CREATE TABLE "projects" (
+    "id" varchar(8) NOT NULL,
+    "name" varchar(100) NOT NULL,
+    "description" text,
+    "client_id" integer NOT NULL,
+    "team_leader_id" integer NOT NULL,
+    "start" timestamp,
+    "end" timestamp,
+    "database_path" varchar(255) NOT NULL,
+    CONSTRAINT "pk_projects_id" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "reports" (
+    "id" serial NOT NULL,
+    "project_id" integer NOT NULL,
+    "type" varchar(50) NOT NULL,
+    "parameters" jsonb,
+    "format" varchar(20) NOT NULL,
+    "path" varchar(255) NOT NULL,
+    "created_at" timestamp NOT NULL,
+    CONSTRAINT "pk_reports_id" PRIMARY KEY ("id")
+);
+
 CREATE TABLE "notifications" (
     "id" serial NOT NULL,
     "user_id" integer NOT NULL,
+    "project_id" varchar NOT NULL,
     "message" text NOT NULL,
     "read" boolean,
     "date" timestamp NOT NULL,
-    "project_id" varchar NOT NULL,
     CONSTRAINT "pk_notifications_id" PRIMARY KEY ("id")
 );
 
-CREATE TABLE "project_members" (
-    "project_id" varchar(8) NOT NULL,
-    "user_id" integer NOT NULL,
-    PRIMARY KEY ("project_id", "user_id")
-);
 -- Foreign key constraints
 -- Schema: public
-ALTER TABLE
-    "project_members"
-ADD
-    CONSTRAINT "fk_project_members_project_id_projects_id" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE CASCADE;
-
-ALTER TABLE
-    "project_members"
-ADD
-    CONSTRAINT "fk_project_members_user_id_users_id" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE;
-
 ALTER TABLE
     "notifications"
 ADD
@@ -96,11 +86,16 @@ ADD
     CONSTRAINT "fk_reports_project_id_projects_id" FOREIGN KEY("project_id") REFERENCES "projects"("id");
 
 ALTER TABLE
-    "notifications"
-ADD
-    CONSTRAINT "fk_notifications_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "users"("id");
-
-ALTER TABLE
     "projects"
 ADD
     CONSTRAINT "fk_projects_team_leader_id_users_id" FOREIGN KEY("team_leader_id") REFERENCES "users"("id");
+
+ALTER TABLE
+    "project_members"
+ADD
+    CONSTRAINT "fk_project_members_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "users"("id");
+
+ALTER TABLE
+    "project_members"
+ADD
+    CONSTRAINT "fk_project_members_project_id_projects_id" FOREIGN KEY("project_id") REFERENCES "projects"("id");
